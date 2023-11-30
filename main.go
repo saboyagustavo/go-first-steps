7ab5340314d64c1f65245cfc642de18db038bbf7
Gustavo Saboya <saboya.gustavo@gmail.com>
2023-11-29 23:44:40 -0300

    structs, types and methods

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+type Product struct {
+	Name        string
+	Descprition string
+	Price       int
+}
+
+func (c Product) GetFullInfo() string {
+	return fmt.Sprintf(
+		"Name: %s,\nDescription: %s,\nPrice: %d,\n",
+		c.Name, c.Descprition, c.Price,
+	)
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, world!"))
 }
+
 func main() {
+	product := Product{
+		Name:        "Golang Course",
+		Descprition: "Learn Golang fast and deploy scalable projects",
+		Price:       100,
+	}
+
 	http.HandleFunc("/", home)
 	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server listening on localhost:8080")
+	fmt.Println(product.GetFullInfo())
 }
