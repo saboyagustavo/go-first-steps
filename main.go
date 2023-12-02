42b03996b6a632d4c8202bae412ab36e9fb68629
Gustavo Saboya <saboya.gustavo@gmail.com>
2023-12-02 15:31:02 -0300

    serving json and giving its properties aliases

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 type Product struct {
-	Name        string
-	Descprition string
-	Price       int
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
 }
 
 func (c Product) GetFullInfo() string {
 	return fmt.Sprintf(
 		"Name: %s,\nDescription: %s,\nPrice: %d,\n",
-		c.Name, c.Descprition, c.Price,
+		c.Name, c.Description, c.Price,
 	)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
-}
-
-func main() {
-	product := Product{
+	course := Product{
 		Name:        "Golang Course",
-		Descprition: "Learn Golang fast and deploy scalable projects",
+		Description: "Learn Golang fast and deploy scalable projects",
 		Price:       100,
 	}
 
+	json.NewEncoder(w).Encode(course)
+}
+
+func main() {
 	http.HandleFunc("/", home)
 	http.ListenAndServe(":8080", nil)
 	fmt.Println("Server listening on localhost:8080")
-	fmt.Println(product.GetFullInfo())
 }
