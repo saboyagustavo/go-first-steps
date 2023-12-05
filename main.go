ea19930088724869de3bf775b6f4c1b6ad9379f7
Gustavo Saboya <saboya.gustavo@gmail.com>
2023-12-05 20:53:21 -0300

    working with channels: concurrency primitives who shares content amongst threads

--- main.go
+++ main.go
@@ -1,52 +1,15 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"time"
-)
+import "fmt"
 
-type Product struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Price       int    `json:"price"`
-}
-
-func (c Product) GetFullInfo() string {
-	return fmt.Sprintf(
-		"Name: %s,\nDescription: %s,\nPrice: %d,\n",
-		c.Name, c.Description, c.Price,
-	)
-}
-
-func counter() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(i + 1)
-		time.Sleep(time.Second)
-	}
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	course := Product{
-		Name:        "Golang Course",
-		Description: "Learn Golang fast and deploy scalable projects",
-		Price:       100,
-	}
-
-	json.NewEncoder(w).Encode(course)
-}
-
+// T1
 func main() {
-	// http.HandleFunc("/", home)
-	// http.ListenAndServe(":8080", nil)
-	// fmt.Println("Server listening on localhost:8080")
+	channel := make(chan string)
 
-	//  counter() // simple execution => prints 3 times 1 -> 10
-	//  counter() // running one after another
-	//  counter() // takes 30 seconds
+	// T2
+	go func() {
+		channel <- "Hello, World!"
+	}()
 
-	go counter() // go routine => prints 3 times the same counter value
-	go counter() // because it's running simultaneously on threads
-	counter()    // takes 10 seconds
+	fmt.Println(<-channel)
 }
